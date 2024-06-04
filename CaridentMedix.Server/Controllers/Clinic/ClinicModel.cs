f2966186a9e4964e5d98860eed9f577a01465432
sabihoshi <sabihoshi.dev@gmail.com>
2024-06-04 19:54:52 +0800

    feat: Add Id to ClinicModel

--- CaridentMedix.Server/Controllers/Clinic/ClinicModel.cs
+++ CaridentMedix.Server/Controllers/Clinic/ClinicModel.cs
@@ -3,24 +3,26 @@
 public class ClinicModel
 {
     public double Distance { get; set; }
- 
+
     public float Latitude { get; init; }
- 
+
     public float Longitude { get; init; }
- 
+
+    public int Id { get; init; }
+
     public List<DentistModel> Dentists { get; init; } = [];
- 
+
     public string Address { get; init; } = null!;
- 
+
     public string Name { get; init; } = null!;
- 
+
     public string? Description { get; init; } = null!;
- 
+
     public string? Email { get; init; } = null!;
- 
+
     public string? ImagePath { get; init; } = null!;
- 
+
     public string? PhoneNumber { get; init; } = null!;
- 
+
     public string? Website { get; init; } = null!;
 }
