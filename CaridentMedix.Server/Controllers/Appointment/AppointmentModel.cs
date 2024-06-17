2c168b8a15f8068de407f491619d08e8104c916d
sabihoshi <sabihoshi.dev@gmail.com>
2024-06-17 21:58:16 +0800

    feat: Add missing properties in AppointmentModel

--- CaridentMedix.Server/Controllers/Appointment/AppointmentModel.cs
+++ CaridentMedix.Server/Controllers/Appointment/AppointmentModel.cs
@@ -6,9 +6,21 @@ public class AppointmentModel
 {
     public ClinicModel Clinic { get; set; } = null!;
 
+    public DateTimeOffset CreatedAt { get; set; } = default!;
+
     public DateTimeOffset ScheduledAt { get; set; }
 
     public DentistModel? Dentist { get; set; }
 
     public int Id { get; set; }
+
+    public string Status { get; set; }
+
+    public string? ClinicCancelMessage { get; set; }
+
+    public string? ClinicMessage { get; set; }
+
+    public string? UserCancelMessage { get; set; }
+
+    public string? UserMessage { get; set; } = null!;
 }
--- CaridentMedix.Server/Controllers/Clinic/DentistModel.cs
+++ CaridentMedix.Server/Controllers/Clinic/DentistModel.cs
@@ -2,9 +2,11 @@
 
 public class DentistModel
 {
+    public int Id { get; init; }
+
     public string Email { get; init; } = null!;
- 
+
     public string Name { get; init; } = null!;
- 
+
     public string PhoneNumber { get; init; } = null!;
 }
